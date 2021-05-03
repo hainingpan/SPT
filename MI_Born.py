b6cae8ec7624e91308e1815aebc5e2e7ade8b140
Haining Pan <jack_pan1994@126.com>
2021-05-03 16:02:24 -0400

    fix MI_Born

--- MI_Born.py
+++ MI_Born.py
@@ -34,22 +34,22 @@ def mutual_info_run_MPI(T,es=100):
 
 def MI_pool(delta,T):
     params=Params(delta=delta,L=64,bc=-1,basis='m',T=T)
-    params.measure_all_born(order='e3')
-    return params.mutual_information_m(np.arange(int(params.L/2)),np.arange(params.L/2)+params.L),params.s_history
+    params.measure_all_born()
+    return params.mutual_information_m(np.arange(params.L//2),np.arange(params.L//2)+params.L),params.s_history
 
 if __name__=="__main__":   
     parser=argparse.ArgumentParser()
-    parser.add_argument('--es',default=100,type=int)
+    parser.add_argument('--es',default=6,type=int)
     args=parser.parse_args()
 
 
     delta_dict={}
     mutual_info_dis_dict={}
     s_history_dis_dict={}
-    T_list=[0]
-    # T_list=np.linspace(0,3e-1,50)
+    # T_list=[0]
+    T_list=np.linspace(0,6e-1,50)
     # T_list=(0,0.01,0.02,0.05,0.1,0.2,0.3)
-    for T in (T_list):
+    for T in T_list:
         st=time.time()
         delta_dict[T],mutual_info_dis_dict[T],s_history_dis_dict[T]=mutual_info_run_MPI(T,args.es)
         print("Time elapsed for {:.4f}: {:.4f}".format(T,time.time()-st))
