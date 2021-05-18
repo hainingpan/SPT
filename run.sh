23d59d035e0d4c299d760b0ea58ac9d911dd5d3a
Haining Pan <jack_pan1994@126.com>
2021-05-18 09:35:30 -0400

    update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-mpirun -n 4 python MI_LN_SSH.py --es 100 --pt 100 --type=onsite --timing=True
-mpirun -n 4 python MI_LN_SSH.py --es 100 --pt 100 --type=link --timing=True
+# mpirun -n 4 python MI_LN_SSH.py --es 100 --pt 100 --type=onsite --timing=True
+mpirun -n 4 python MI_LN_SSH.py --es 100 --pt 500 --type=link --timing=True
+# mpirun -n 4 python MI_LN_SSH.py --es 100 --pt 100 --type=onsite --timing=True --Bp=True
+mpirun -n 4 python MI_LN_SSH.py --es 100 --pt 500 --type=link --timing=True --Bp=True
