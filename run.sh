c810fe1b3994f12a8b8850c60fa21a5fc416bfe4
Haining Pan <jack_pan1994@126.com>
2021-05-19 22:13:49 -0400

    fix run.sh

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@
 # mpirun -n 4 python MI_LN_SSH.py --es 100 --pt 100 --type=onsite --timing=True --Bp=True
 # mpirun -n 4 python MI_LN_SSH.py --es 100 --pt 500 --type=link --timing=True --Bp=True
 
-mpirun -np 4 python -m mpi4py.futures MI_LN_inf_SSH.py --es 100 --min 8 --max 100 --timing True --type=onsite
-mpirun -np 4 python -m mpi4py.futures MI_LN_inf_SSH.py --es 1000 --min 8 --max 100 --timing True --type=link
+mpirun -np 4 python -m mpi4py.futures MI_LN_inf_SSH.py --es 100 --min 8 --max 100 --timing True --type onsite
+mpirun -np 4 python -m mpi4py.futures MI_LN_inf_SSH.py --es 1000 --min 8 --max 100 --timing True --type link
