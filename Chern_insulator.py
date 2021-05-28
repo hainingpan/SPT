3bad80d4dafe8daa1cdd83c298026b2061f087d5
Haining Pan <jack_pan1994@126.com>
2021-05-28 00:37:20 -0400

    add Chern_insulator.py

---
+++ Chern_insulator.py
@@ -0,0 +1,315 @@
+import numpy as np
+import scipy.linalg as la
+import numpy.linalg as nla
+import numpy.matlib
+
+
+class Params:
+    def __init__(self,
+            Lx=4,
+            Ly=16,
+            t=1,
+            Delta=1,
+            m=1,
+            bcx=1,
+            bcy=-1,
+            T=0,
+            history=False):
+        self.Lx = Lx
+        self.Ly = Ly
+        self.t = t
+        self.Delta = Delta
+        self.m = m
+        self.bcx = bcx
+        self.bcy = bcy
+        self.T = T
+        self.history = history
+        self.sigmax = np.array([[0, 1], [1, 0]])
+        self.sigmay = np.array([[0, -1j], [1j, 0]])
+        self.sigmaz = np.array([[1, 0], [0, -1]])
+        # check which one is faster, use sparse or dense?
+        hopx = np.diag(np.ones(Lx-1), -1)
+        hopx[0, -1] = bcx
+        hopy = np.diag(np.ones(Ly-1), -1)
+        hopy[0, -1] = bcy
+        hopxmat = np.kron(hopx, np.eye(Ly))
+        hopymat = np.kron(np.eye(Lx), hopy)
+        onsitemat = np.eye(Lx*Ly)
+        self.Hamiltonian = ((np.kron(hopxmat-hopxmat.T, self.sigmax)+np.kron(hopymat-hopymat.T, self.sigmay)) *
+                            1j*t+Delta*np.kron(hopxmat+hopxmat.T+hopymat+hopymat.T, self.sigmaz))/2+m*np.kron(onsitemat, self.sigmaz)
+
+    def bandstructure(self):
+        val, vec = nla.eigh(self.Hamiltonian)
+        sortindex = np.argsort(val)
+        self.val = val[sortindex]
+        self.vec = vec[:, sortindex]
+
+    def fermi_dist(self, energy, E_F):
+        if self.T == 0:
+            return np.heaviside(E_F-energy, 0)
+        else:
+            return 1/(1+np.exp((energy-E_F)/self.T))
+
+    def correlation_matrix(self, E_F=0):
+        '''
+        G_{ij}=<f_i^\dagger f_j>
+        '''
+        if not (hasattr(self, 'val') and hasattr(self, 'vec')):
+            self.bandstructure()
+        occupancy_mat = np.matlib.repmat(
+            self.fermi_dist(self.val, E_F), self.vec.shape[0], 1)
+        self.C_f = np.real((occupancy_mat*self.vec)@self.vec.T.conj())
+
+    def covariance_matrix(self, E_F=0):
+        '''
+        c.f. notes
+        Maybe differs by a minus sign
+        '''
+        if not hasattr(self, 'C_f'):
+            if self.Lx < np.inf:
+                self.correlation_matrix()
+            else:
+                self.correlation_matrix_inf_fft()
+        G = self.C_f
+        Gamma_11 = 1j*(G-G.T)
+        Gamma_21 = -(np.eye(G.shape[0])-G-G.T)
+        Gamma_12 = -Gamma_21.T
+        Gamma_22 = -1j*(G.T-G)
+        Gamma = np.zeros((2*G.shape[0], 2*G.shape[0]), dtype=complex)
+        even = np.arange(2*G.shape[0])[::2]
+        odd = np.arange(2*G.shape[0])[1::2]
+        Gamma[np.ix_(even, even)] = Gamma_11
+        Gamma[np.ix_(even, odd)] = Gamma_12
+        Gamma[np.ix_(odd, even)] = Gamma_21
+        Gamma[np.ix_(odd, odd)] = Gamma_22
+        assert np.abs(np.imag(Gamma)).max(
+        ) < 1e-10, "Covariance matrix not real"
+        self.C_m = np.real(Gamma-Gamma.T.conj())/2
+        self.C_m_history = [self.C_m]
+
+    def linearize_index(self, subregion, n, proj=False):
+        '''
+        If proj ==True, then the index used for projection operator will be returned
+        '''
+        subregion_x, subregion_y = (subregion)
+        subregion_x = np.array(subregion_x)
+        subregion_y = np.array(subregion_y)
+        X, Y = np.meshgrid(subregion_x, subregion_y)
+        linear_index = ((X*self.Ly+Y).flatten('F'))
+        if proj:
+            return sorted(np.concatenate([n*linear_index+i for i in range(0, n, 2)]))
+        else:
+            return sorted(np.concatenate([n*linear_index+i for i in range(n)]))
+
+    def c_subregion_f(self, subregion, linear=True):
+        '''
+        subregion: [subregoin_x, subregion_y] index of unit cell
+        '''
+        if not hasattr(self, 'C'):
+            self.covariance_matrix()
+        try:
+            subregion = list(subregion)
+        except:
+            raise ValueError("The subregion is ill-defined"+subregion)
+
+        if len(subregion) == 2:
+            if isinstance(subregion[0], int) and isinstance(subregion[1], int):
+                linear = True
+            elif (not isinstance(subregion[0], int)) and (not isinstance(subregion[1], int)):
+                linear = False
+            else:
+                raise ValueError('Illegal subregion')
+        else:
+            linear = True
+        if linear:
+            # linearized index
+            subregion_index = subregion
+        else:
+            # 2D index
+            subregion_index = self.linearize_index(subregion, 2)
+        # This is a dumb way...
+        return self.C_f[np.ix_(subregion_index, subregion_index)]
+
+    def von_Neumann_entropy_f(self, subregion):
+        c_A = self.c_subregion_f(subregion)
+        val = nla.eigvalsh(c_A)
+        self.val_sh = val
+        val = np.sort(val)
+        return np.real(-np.sum(val*np.log(val+1e-18j))-np.sum((1-val)*np.log(1-val+1e-18j)))
+
+    def c_subregion_m(self, subregion, Gamma=None):
+        '''
+        subregion: [subregoin_x, subregion_y] index of unit cell
+        '''
+        if not hasattr(self, 'C_m'):
+            self.covariance_matrix()
+        if Gamma is None:
+            Gamma = self.C_m_history[-1]
+        try:
+            subregion = list(subregion)
+        except:
+            raise ValueError("The subregion is ill-defined"+subregion)
+
+        if len(subregion) == 2:
+            if isinstance(subregion[0], int) and isinstance(subregion[1], int):
+                linear = True
+            elif (not isinstance(subregion[0], int)) and (not isinstance(subregion[1], int)):
+                linear = False
+            else:
+                raise ValueError('Illegal subregion')
+        else:
+            linear = True
+        if linear:
+            # linearized index
+            subregion_index = subregion
+        else:
+            # 2D index
+            subregion_index = self.linearize_index(subregion, 4)
+
+        return Gamma[np.ix_(subregion_index, subregion_index)]
+
+    def von_Neumann_entropy_m(self, subregion):
+        c_A = self.c_subregion_m(subregion)
+        val = nla.eigvalsh(1j*c_A)
+        self.val_sh = val
+        val = np.sort(val)
+        val = (1-val)/2+1e-18j  # \lambda=(1-\xi)/2
+        return np.real(-np.sum(val*np.log(val))-np.sum((1-val)*np.log(1-val)))/2
+
+    def mutual_information_f(self, subregion_A, subregion_B):
+        subregion_A = self.linearize_index(subregion_A, 2)
+        subregion_B = self.linearize_index(subregion_B, 2)
+        s_A = self.von_Neumann_entropy_f(subregion_A)
+        s_B = self.von_Neumann_entropy_f(subregion_B)
+        assert np.intersect1d(
+            subregion_A, subregion_B).size == 0, "Subregion A and B overlap"
+        subregion_AB = np.concatenate([subregion_A, subregion_B])
+        s_AB = self.von_Neumann_entropy_f(subregion_AB)
+        return s_A+s_B-s_AB
+
+    def mutual_information_m(self, subregion_A, subregion_B):
+        subregion_A = self.linearize_index(subregion_A, 4)
+        subregion_B = self.linearize_index(subregion_B, 4)
+        assert np.intersect1d(
+            subregion_A, subregion_B).size == 0, "Subregion A and B overlap"
+        s_A = self.von_Neumann_entropy_m(subregion_A)
+        s_B = self.von_Neumann_entropy_m(subregion_B)
+        subregion_AB = np.concatenate([subregion_A, subregion_B])
+        s_AB = self.von_Neumann_entropy_m(subregion_AB)
+        return s_A+s_B-s_AB
+
+    def log_neg(self, subregion_A, subregion_B, Gamma=None):
+        subregion_A = self.linearize_index(subregion_A, 4)
+        subregion_B = self.linearize_index(subregion_B, 4)
+        assert np.intersect1d(
+            subregion_A, subregion_B).size == 0, "Subregion A and B overlap"
+        if not hasattr(self, 'C_m'):
+            self.covariance_matrix()
+
+        if Gamma is None:
+            Gamma = self.C_m_history[-1]
+        subregion_A = np.array(subregion_A)
+        subregion_B = np.array(subregion_B)
+        Gm_p = np.block([
+            [-Gamma[np.ix_(subregion_A, subregion_A)], 1j *
+             Gamma[np.ix_(subregion_A, subregion_B)]],
+            [1j*Gamma[np.ix_(subregion_B, subregion_A)],
+             Gamma[np.ix_(subregion_B, subregion_B)]]
+        ])
+        Gm_n = np.block([
+            [-Gamma[np.ix_(subregion_A, subregion_A)], -1j *
+             Gamma[np.ix_(subregion_A, subregion_B)]],
+            [-1j*Gamma[np.ix_(subregion_B, subregion_A)],
+             Gamma[np.ix_(subregion_B, subregion_B)]]
+        ])
+        idm = np.eye(Gm_p.shape[0])
+        # Gm_x=idm-(idm+1j*Gm_p)@nla.inv(idm-Gm_n@Gm_p)@(idm+1j*Gm_n)
+        Gm_x = idm-(idm+1j*Gm_p)@(la.solve((idm-Gm_n@Gm_p), (idm+1j*Gm_n)))
+        Gm_x = (Gm_x+Gm_x.T.conj())/2
+        xi = nla.eigvalsh(Gm_x)
+        subregion_AB = np.concatenate([subregion_A, subregion_B])
+        eA = np.sum(np.log(((1+xi+0j)/2)**0.5+((1-xi+0j)/2)**0.5))/2
+        chi = nla.eigvalsh(1j*Gamma[np.ix_(subregion_AB, subregion_AB)])
+        sA = np.sum(np.log(((1+chi)/2)**2+((1-chi)/2)**2))/4
+        return np.real(eA+sA)
+
+    def projection(self, s):
+        '''
+        For type:'onsite'
+            occupancy number: s= 0,1 
+            (-1)^0 even parity, (-1)^1 odd parity
+        For type:'link'
+            (o,+)|(o,-)|(e,+)|(e,-)
+        '''
+        assert (s == 0 or s == 1), "s={} is either 0 or 1".format(s)
+        blkmat = np.array([[0, -(-1)**s, 0, 0],
+                           [(-1)**s, 0, 0, 0],
+                           [0, 0, 0, (-1)**s],
+                           [0, 0, -(-1)**s, 0]])
+        return blkmat
+
+    def measure(self, s, ix):
+        if not hasattr(self, 'C_m'):
+            self.covariance_matrix()
+        if not hasattr(self, 's_history'):
+            self.s_history = []
+        if not hasattr(self, 'i_history'):
+            self.i_history = []
+
+        mat = self.C_m_history[-1].copy()
+
+        for i_ind, i in enumerate(ix):
+            mat[[i, -(len(ix)-i_ind)]] = mat[[-(len(ix)-i_ind), i]]
+            mat[:, [i, -(len(ix)-i_ind)]] = mat[:, [-(len(ix)-i_ind), i]]
+
+        self.mat = mat
+
+        Gamma_LL = mat[:-len(ix), :-len(ix)]
+        Gamma_LR = mat[:-len(ix), -len(ix):]
+        Gamma_RR = mat[-len(ix):, -len(ix):]
+
+        proj = self.projection(s)
+        Upsilon_LL = proj[:-len(ix), :-len(ix)]
+        Upsilon_RR = proj[-len(ix):, -len(ix):]
+        Upsilon_RL = proj[-len(ix):, :-len(ix)]
+        zero = np.zeros((mat.shape[0]-len(ix), len(ix)))
+        zero0 = np.zeros((len(ix), len(ix)))
+        mat1 = np.block([[Gamma_LL, zero], [zero.T, Upsilon_RR]])
+        mat2 = np.block([[Gamma_LR, zero], [zero0, Upsilon_RL]])
+        mat3 = np.block([[Gamma_RR, np.eye(len(ix))],
+                        [-np.eye(len(ix)), Upsilon_LL]])
+        self.mat2 = mat2
+        if np.count_nonzero(mat2):
+            Psi = mat1+mat2@(la.solve(mat3, mat2.T))
+            # Psi=mat1+mat2@(la.lstsq(mat3,mat2.T)[0])
+            assert np.abs(
+                np.trace(Psi)) < 1e-5, "Not trace zero {:e}".format(np.trace(Psi))
+        else:
+            Psi = mat1
+
+        for i_ind, i in enumerate(ix):
+            Psi[[i, -(len(ix)-i_ind)]] = Psi[[-(len(ix)-i_ind), i]]
+            Psi[:, [i, -(len(ix)-i_ind)]] = Psi[:, [-(len(ix)-i_ind), i]]
+        Psi = (Psi-Psi.T)/2   # Anti-symmetrize
+        if self.history:
+            self.C_m_history.append(Psi)
+            self.s_history.append(s)
+            self.i_history.append(i)
+        else:
+            self.C_m_history = [Psi]
+            self.s_history = [s]
+            self.i_history = [i]
+
+    def measure_all_Born(self, proj_range):
+        proj_range = self.linearize_index(proj_range, 4, proj=True)
+        # print(proj_range)
+        self.P_0_list = []
+        self.covariance_matrix()
+        for i in proj_range:
+            P_0 = (self.C_m_history[-1][i, i+1]+1)/2
+            self.P_0_list.append(P_0)
+            if np.random.rand() < P_0:
+                self.measure(0, [i, i+1])
+            else:
+                self.measure(1, [i, i+1])
+        return self
