ec99f2d20211cbf2091d5692eb2a2511e965c839
Haining Pan <jack_pan1994@126.com>
2021-08-02 11:24:37 -0400

    add Bp to inf_T_SSH

--- inf_T_SSH.py
+++ inf_T_SSH.py
@@ -9,12 +9,12 @@ import numpy as np
 from mpi4py.futures import MPIPoolExecutor
 
 def run(p):
-    L,ty=p
+    L,ty,Bp=p
     params=Params(delta=0,L=L,T=np.inf,E0=0,dE=1)
-    if ty=='onsite':
-        params.measure_all_Born(type='onsite')
-    elif ty=='link':
-        params.measure_all_Born(type='link')
+    if ty!='no':
+        params.measure_all_Born(type=ty)
+        if Bp:
+            params.measure_all_Born(proj_range=np.arange(params.L*3,params.L*4,2*(ty=='onsite')+4*(ty=='link')),type=ty)
     LN=params.log_neg(np.arange(L),np.arange(L)+2*L)
     MI=params.mutual_information_m(np.arange(L),np.arange(L)+2*L)
     return MI,LN,params.E_mean
@@ -25,6 +25,7 @@ if __name__=="__main__":
     # parser.add_argument('--Lmin',default=16,type=int)
     # parser.add_argument('--Lmax',default=128,type=int)
     parser.add_argument('--type',default='no',type=str)
+    parser.add_argument('--Bp',default=False,type=bool)
     args=parser.parse_args()
     executor=MPIPoolExecutor()
     L_list=[16,32,64,96,128,128+64,256]
@@ -32,7 +33,7 @@ if __name__=="__main__":
     MI_dict={}
     E_mean_dict={}
     for L in L_list:
-        inputs=inputs=[(L,args.type) for _ in range(args.es)]
+        inputs=inputs=[(L,args.type,args.Bp) for _ in range(args.es)]
         pool=executor.map(run,inputs)
         LN_dict[L]=[]
         MI_dict[L]=[]
@@ -45,17 +46,5 @@ if __name__=="__main__":
         
     executor.shutdown()
 
-    with open('inf_T_SSH_{:s}.pickle'.format(args.type),'wb') as f:
+    with open('inf_T_SSH_{:s}_Ap{:s}.pickle'.format(args.type,args.Bp*'Bp'),'wb') as f:
         pickle.dump([MI_dict,LN_dict,E_mean_dict],f)
-    
-
-
-    
-
-
-
-
-
-
-    
-
