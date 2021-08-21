83e8910bddbaa9a29794a69c7fd7c2ef6948e31b
Haining Pan <jack_pan1994@126.com>
2021-08-20 20:46:09 -0700

    update MI_Born.py

--- MI_Born.py
+++ MI_Born.py
@@ -11,32 +11,37 @@ from mpi4py.futures import MPIPoolExecutor
 
 def mutual_info_run_MPI(T,es,L,mtype):
     delta_list=np.linspace(-1,1,50)
+    mutual_info_dis_list=[]
     log_neg_dis_list=[]
     ensemblesize=es
 
     for delta in delta_list:
         log_neg_ensemble_list=[]
+        mutual_info_ensemble_list=[]
         mutual_info_ensemble_list_pool=[]
         executor=MPIPoolExecutor()
         inputs=[(delta,T,L,mtype) for _ in range(ensemblesize)]
         mutual_info_ensemble_list_pool=executor.map(MI_pool,inputs)
         executor.shutdown()
         for result in mutual_info_ensemble_list_pool:
-            LN=result
+            MI,LN=result
+            mutual_info_ensemble_list.append(MI)
             log_neg_ensemble_list.append(LN)
+        mutual_info_dis_list.append(mutual_info_ensemble_list)
         log_neg_dis_list.append(log_neg_ensemble_list)
 
     return delta_list,log_neg_dis_list
 def MI_pool(p):
     delta,T,L,mtype=p
-    params=Params(delta=delta,L=L,bc=-1,basis='m',T=T)
+    params=Params(delta=delta,L=L,bc=-1,basis='f',T=T)
     if mtype=='Born':
         params.measure_all_Born()
     elif mtype=='1':
         params.measure_all(0)
     
-    LN=params.log_neg(np.arange(params.L//2),np.arange(params.L//2)+params.L)
-    return LN
+    MI=params.mutual_information_m(np.arange(params.L//4),np.arange(params.L//4)+params.L//2)
+    LN=params.log_neg(np.arange(params.L//4),np.arange(params.L//4)+params.L//2)
+    return MI,LN
 
 if __name__=="__main__":   
     parser=argparse.ArgumentParser()
